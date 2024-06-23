2e23c8af58c541a526a5eba8711a63cffa135acb
Nayan Kr Bera <nayanbera9@gmail.com>
2024-06-23 23:19:57 +0530

    first commit

---
+++ src/components/Projects.jsx
@@ -0,0 +1,56 @@
+import { PROJECTS } from "../constants"
+import { motion } from "framer-motion"
+const Projects = () => {
+  return (
+    <div className="border-b border-neutral-900 pb-24">
+      <motion.h1 
+      whileInView={{opacity:1, y:0}}
+      initial={{opacity:0,y:-100}}
+      transition={{duration:0.5}}
+    
+      className=" my-20 text-center text-4xl">Projects</motion.h1>
+      <div>
+        {PROJECTS.map((project, index)=>(
+          <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
+            <div className=" w-full lg:w-1/4">
+            <motion.img
+            whileInView={{opacity:1, x:0}}
+            initial={{opacity:0,x:-100}}
+            transition={{duration:1}}
+
+            src={project.image}
+            width={150}
+            height={150}
+             alt={project.title}
+             className="mb-6 rounded"
+              />
+            </div>
+            <motion.div
+             whileInView={{opacity:1, x:0}}
+             initial={{opacity:0,x:100}}
+             transition={{duration:1}}
+            className=" w-full max-w-xl lg:w-3/4">
+            <h6 className=" mb-2 font-semibold">{project.title}</h6>
+            <p className=" mb-4 text-neutral-400">{project.description}</p>
+            {project.technologies.map((tech,index)=>(
+              <motion.span key={index}
+              whileInView={{opacity:1, x:0}}
+              initial={{opacity:0,x:100}}
+              transition={{duration:2}}
+              
+              className=" mr2 rounded bg-neutral-900 px-2 py-1
+               text-sm font-medium bg-gradient-to-r from-green-300
+            to-gray-500 bg-clip-text  tracking-tight text-transparent"
+              >
+                {tech}
+                 </motion.span>
+            ))}
+            </motion.div>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Projects
